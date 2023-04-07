541232d9cecd083ddc7dd8ca53cd50608b874362
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2023-04-07 18:05:10 -0300

    Registrando exibição dos livros

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { VolumeInfo } from './../../models/interfaces';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,14 +9,40 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy{
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = '';
+  subscription: Subscription;
+  livro: Livro;
 
   constructor(private service: LivroService) { }
 
   buscarLivros(){
-     this.service.buscar(this.campoBusca).subscribe((retornoAPI) => console.log(retornoAPI), (error) => console.log(error))
+     this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (items) => {this.listaLivros = this.livrosResultadoParaLivro(items)},
+      // A declaração error e complete pode ser omitidas.
+      error: erro => console.log(erro),
+     })
+  }
+
+  livrosResultadoParaLivro(items): Livro[]{
+    const livros: Livro[] = []
+    items.forEach(item => {
+        livros.push(this.livro = {
+        title: item.VolumeInfo?.title,
+        authors: item.VolumeInfo?.authors,
+        publisher: item.VolumeInfo?.publisher,
+        publishedDate: item.VolumeInfo?.publishedDate,
+        description: item.VolumeInfo?.description,
+        previewLink: item.VolumeInfo?.previewLink,
+        thumbnail: item.VolumeInfo?.imageLinks?.thumbnail
+      })
+    })
+    return livros
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
   }
 }
